cb15833180a52acd726fa327771ba55c20733424
Reo Yamanaka <ryamanaka807@gmail.com>
2021-02-02 02:47:07 -0500

    added header statement in script

--- script.py
+++ script.py
@@ -1,8 +1,6 @@
 sample_list = [1, 2, 3, 4, "p", "r", "j", 0]
-
+print("Dynamically removing all elements from the list %s..."%sample_list) 
 counter = len(sample_list) - 1
-
-print(sample_list)
 
 while counter > -1:
     del sample_list[counter]
